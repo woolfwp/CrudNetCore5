ab4cedf2726aa321399893fa28b3fc0a9dbb210d
Sebastian Gomez <sebasgomez1099@gmail.com>
2022-05-05 15:41:51 -0500

    actualizando a repositorio git

---
+++ CrudNetCore5Practica/CrudNetCore5Practica/Controllers/LibrosController.cs
@@ -0,0 +1,27 @@
+﻿using CrudNetCore5Practica.Data;
+using CrudNetCore5Practica.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5Practica.Controllers
+{
+    public class LibrosController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public LibrosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Libro> listLibro = _context.Libro;
+            return View(listLibro);
+        }
+    }
+}
---
+++ CrudNetCore5Practica/CrudNetCore5Practica/Models/Libro.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudNetCore5Practica.Models
+{
+    public class Libro
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El título es obligatorio")]
+        [StringLength(50,ErrorMessage = "el {0} debe ser al menos de {2} y maximo de {1} caracteres",MinimumLength = 3)]
+        [Display(Name = "Título")]
+        public string Titulo { get; set; }
+
+        [Required(ErrorMessage = "La Descripción es obligatoria")]
+        [StringLength(100, ErrorMessage = "el {0} debe ser al menos de {2} y maximo de {1} caracteres", MinimumLength = 25)]
+        [Display(Name = "Descripción")]
+        public String Descripcion { get; set; }
+
+        [Required(ErrorMessage = "La Fecha es obligatoria")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Fecha de lanzamiento")]
+        public DateTime FechaLanzamiento { get; set; }
+
+        [Required(ErrorMessage = "El Autor es obligatorio")]
+        public string Autor { get; set; }
+
+        [Required(ErrorMessage = "El Precio es obligatorio")]
+        public int Precio { get; set; }
+    }
+}
